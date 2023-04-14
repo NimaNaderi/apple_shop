e722f1bc974b6306240931ba0e3ac6e1e5bae230
Nima Naderi <nima.er.84@gmail.com>
2023-04-14 05:02:47 +0430

    Horizontal Product Category List Added.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/animation.dart';
 
 class CustomColors {
   static const blueIndicator = Color(0xff3B5EDF);
+  static const backgroundScreenColor = Color(0xffEEEEEE);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apple_shop/constants/colors.dart';
 import 'package:apple_shop/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,13 +14,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(428, 926),
-      builder: (context, child) => const MaterialApp(
+      builder: (context, child) => MaterialApp(
         home: Scaffold(
+          backgroundColor: CustomColors.backgroundScreenColor,
           body: SafeArea(
-            child: BannerSlider(),
+            child: Center(
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44.w),
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) => Padding(
+                      padding: EdgeInsets.only(left: 20.w),
+                      child: CategoryHorizontalItemList(),
+                    ),
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class CategoryHorizontalItemList extends StatelessWidget {
+  const CategoryHorizontalItemList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 56.h,
+              width: 56.w,
+              decoration: ShapeDecoration(
+                color: Colors.red,
+                shadows: const [
+                  BoxShadow(
+                    blurRadius: 25,
+                    spreadRadius: -12,
+                    offset: Offset(0, 16),
+                    color: Colors.red,
+                  )
+                ],
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.r),
+                ),
+              ),
+            ),
+            Icon(
+              Icons.mouse,
+              size: 32.h,
+              color: Colors.white,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        Text(
+          'همه',
+          style: TextStyle(fontFamily: 'SB', fontSize: 12.sp),
+        ),
+      ],
     );
   }
 }
