dfbf3c7211cc0988d5dc460bee72fa989ff35940
Nima Naderi <nima.er.84@gmail.com>
2023-04-16 04:54:36 +0430

    Login Logic Added.

---
+++ lib/di/di.dart
@@ -0,0 +1,21 @@
+import 'package:apple_shop/data/datasource/authentication_datasource.dart';
+import 'package:apple_shop/data/repository/authentication_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final locator = GetIt.instance;
+Future<void> getItInit() async {
+
+  // Components
+  locator.registerSingleton<Dio>(
+      Dio(BaseOptions(baseUrl: 'http://startflutter.ir/api/')));
+
+  locator.registerSingleton<SharedPreferences>(await SharedPreferences.getInstance());
+
+  // DataSources
+  locator.registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
+
+  // Repositories
+  locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
+}
